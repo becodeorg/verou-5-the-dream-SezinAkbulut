3d200e0e8b9e44fd509d836515718636c84b90a7
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-03 11:22:35 +0100

    add a currency convert function

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+/*
 function myCalculator($AED, $oper, $EU) {
 $sum;
  switch ($oper) {
@@ -20,3 +21,18 @@ $oper = $_GET["oper"];
 $EU = $_GET["EU"];
 
 echo "Result: " . myCalculator($AED, $oper, $EU);
+*/
+
+
+if (isset($_GET["submit"])) {
+    $cc_input = $_GET["input"];
+    $cc_dropdown = $_GET["dropdown"];
+
+    if ($cc_dropdown == "EU") {
+        $output = $cc_input * 0.2485;
+        echo "<h1> " . number_format($output, 2) . " Euro" . "</h1>";
+    } else if ($cc_dropdown == "AED") {
+        $output = $cc_input * 4.0237;
+        echo "<h1> " . number_format($output, 2) . " Dubai Dirham" . "</h1>";
+    }
+}
--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <title>The Dream</title>
 </head>
 <body>
+<h1>EUR/AED Calculator</h1>
+<p>Covert Dubai Dirham to Euro for next Dubai Trip</p>
 <form action="functions.php" method="get">
-    <input type="text" name="AED" placeholder="AED">
+  <!--  <input type="text" name="AED" placeholder="AED">
     <select name="oper">
         <label>Choose currency</label>
         <option value="add">Add</option>
@@ -14,6 +16,17 @@
     </select>
     <input type="text" name="EU" placeholder="EU">
     <button type="submit">Calculate!</button>
+    -->
+    <label for="input">Enter Currency:</label>
+    <input type="text" name="input" placeholder="Enter Currency">
+    <label for="dropdown">Choose currency:</label>
+    <select name="dropdown">
+        <option value="AED">AED</option>
+        <option value="EU">EU</option>
+    </select>
+    <button type="submit" name="submit" value="Convert">Convert</button>
 </form>
 </body>
 </html>
+
+
