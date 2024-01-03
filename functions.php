1db95b9a64ff17cc92c479ed040e36f2fa20ed8e
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-03 10:43:11 +0100

    add a function in functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,22 @@
 <?php
+function myCalculator($AED, $oper, $EU) {
+$sum;
+ switch ($oper) {
+     case "add":
+         $sum = $AED + $EU;
+         break;
+     case  "sub":
+         $sum = $AED - $EU;
+         break;
+     default:
+         $sum = "There was an error!";
+         break;
+ }
+ return $sum;
+}
+
+$AED = $_GET["AED"];
+$oper = $_GET["oper"];
+$EU = $_GET["EU"];
+
+echo "Result: " . myCalculator($AED, $oper, $EU);
